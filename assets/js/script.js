bd2753c3448fce089d46660caa6277552b4cb36c
Abitagz <helloabita2005@yahoo.com>
2021-08-27 10:35:02 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
-    let button = document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons){
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit");
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
-})
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -30,7 +33,7 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
-        throw `unknown game type: ${gameType}. Aborting`;
+        throw `unknown game type: ${gameType}. Aborting!`;
     }
 
 }
